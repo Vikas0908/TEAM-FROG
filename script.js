3685a4a97dabc7ac5ae2cdcde3bf12f48ed55df1
Vikas Kumar <109017300+Vikas0908@users.noreply.github.com>
2023-04-07 10:04:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+function first(){
+    document.getElementById("slideimage").src="modi2.jpg";
+}
+function second(){
+    document.getElementById("slideimage").src="modi3.jpg";
+}
+function third(){
+    document.getElementById("slideimage").src="modi4.jpg";
+}
+function fourth(){
+    document.getElementById("slideimage").src="modi1.jpg";
+}
+// function fifth(){
+//     document.getElementById("slideimage").src="modi3.jpg"
+// }
+setInterval( 1000);
+setInterval(second,2000);
+setInterval(third,3000);
+setInterval(fourth,4000);
+// setInterval(fifth,10000);
